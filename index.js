4953fff99122123364bdd6b43c02a16e28ff99ad
Esther <eagyare91@gmail.com>
2021-01-21 00:18:22 +0000

    styling

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 const buttons = document.querySelectorAll('.btn')
 
 
-
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function() {
-    
-        var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-    });
-  }
