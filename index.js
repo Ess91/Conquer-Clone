ad79b4dd16d99467feb27d12b1b6acfedbb215ac
Esther <eagyare91@gmail.com>
2021-01-21 00:35:01 +0000

    active class

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const buttons = document.querySelectorAll('.btn')
+const ul = document.querySelector('ul');
+const a = document.querySelectorAll('a');
+
+a.forEach(element => {
+    element.addEventListener('click', function() {
+        ul.querySelector('.active').classList.remove('active');
+
+        element.classList.add('active');
+    });
+});
 
 
